78c6abcc4fab0838464908d1889442af71d5d03a
Hyungseock-Yun <bagzein@gmail.com>
2023-11-29 23:56:33 +0900

    - 스프링에서 통합하여 사용하는 외부 설정

---
+++ external-start/src/main/java/hello/EnviromentCheck.java
@@ -0,0 +1,31 @@
+package hello;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EnviromentCheck {
+
+  private final Environment env;
+
+  public EnviromentCheck(Environment env) {
+    this.env = env;
+  }
+
+  /**
+   * VM option과 Program arguments 어느 것을 사용해도 설정값을 가져온다.
+   * Program arguments가 우선. (1. 더 유연하고  2. 접근 범위가 더 좁기 때문)
+   */
+  @PostConstruct
+  public void init() {
+    String url = env.getProperty("url");
+    String username = env.getProperty("username");
+    String password = env.getProperty("password");
+    log.info("env url={}", url);
+    log.info("username={}", username);
+    log.info("password={}", password);
+  }
+}
